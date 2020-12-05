d00761751d31055a3f49a0963e7b2dc803f443df
Qamal Kosim-Satyaputra <qamal@deputy.com>
2020-12-05 16:42:46 +1100

    Day 5

--- cmd/day5.go
+++ cmd/day5.go
@@ -11,7 +11,72 @@ var day5Cmd = &cobra.Command{
 	Use: "day5",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("day5 called")
+		if len(args) != 1 {
+			panic("missing input file")
+		}
+		lines, err := FileToLines(args[0])
+		if err != nil {
+			panic(err)
+		}
+
+		max := 0
+		seats := [128][8]int{}
+		for _, line := range lines {
+			lastx := ""
+			lasty := ""
+			ymin := 0
+			ymax := 128
+			xmin := 0
+			xmax := 8
+			for _, c := range line {
+				if string(c) == "F" {
+					ymax -= (ymax - ymin) / 2
+					lasty = string(c)
+				} else if string(c) == "B" {
+					ymin += (ymax - ymin) / 2
+					lasty = string(c)
+				} else if string(c) == "L" {
+					xmax -= (xmax - xmin) / 2
+					lastx = string(c)
+				} else if string(c) == "R" {
+					xmin += (xmax - xmin) / 2
+					lastx = string(c)
+				}
+			}
+
+			row := ymin
+			col := xmin
+			if lastx == "R" {
+				col = xmax - 1
+			} else if lasty == "B" {
+				row = ymax - 1
+			}
+
+			id := row*8 + col
+			if id > max {
+				max = id
+			}
+			seats[row][col] = 1
+			// debug
+			// fmt.Printf("%d, %d, %d, %d = [%d, %d, %d]\n", ymin, ymax, xmin, xmax, row, col, id)
+		}
+		fmt.Printf("Max: %d\n", max)
+
+		// find missing seat
+		for i, rows := range seats {
+			sum := 0
+			missingCol := 0
+			for j, col := range rows {
+				if col == 0 {
+					missingCol = j
+				} else {
+					sum += 1
+				}
+			}
+			if sum == 7 {
+				fmt.Printf("Missing [%d, %d] = id: %d\n", i, missingCol, i*8+missingCol)
+			}
+		}
 	},
 }
 
