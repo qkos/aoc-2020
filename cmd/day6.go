ca439978f7cdeb3490dff7c56b003176ff98f428
Qamal Kosim-Satyaputra <qamal@deputy.com>
2020-12-06 16:18:18 +1100

    Day 6

--- cmd/day6.go
+++ cmd/day6.go
@@ -11,8 +11,54 @@ var day6Cmd = &cobra.Command{
 	Use: "day6",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("day6 called")
+		if len(args) != 1 {
+			panic("missing input file")
+		}
+		lines, err := FileToLines(args[0])
+		if err != nil {
+			panic(err)
+		}
+
+		sum := 0
+		allAnswered := 0
+		gs := 0
+		mp := map[string]int{}
+		for _, line := range lines {
+			if line == "" {
+				// reset
+				sum += len(mp)
+				allAnswered += all(mp, gs)
+				mp = map[string]int{}
+				gs = 0
+				continue
+			}
+
+			// add to map
+			for _, c := range line {
+				cs := string(c)
+				if _, ok := mp[cs]; !ok {
+					mp[cs] = 1
+				} else {
+					mp[cs] += 1
+				}
+			}
+			gs++
+		}
+		sum += len(mp)
+		allAnswered += all(mp, gs)
+
+		fmt.Printf("Sum: %d\n", sum)
+		fmt.Printf("All: %d\n", allAnswered)
 	},
+}
+
+func all(mp map[string]int, gs int) (ttl int) {
+	for _, v := range mp {
+		if v >= gs {
+			ttl++
+		}
+	}
+	return
 }
 
 func init() {
--- cmd/day8.go
+++ cmd/day8.go
@@ -11,7 +11,17 @@ var day8Cmd = &cobra.Command{
 	Use: "day8",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("day8 called")
+		if len(args) != 1 {
+			panic("missing input file")
+		}
+		lines, err := FileToLines(args[0])
+		if err != nil {
+			panic(err)
+		}
+
+		for l, line := range lines {
+			fmt.Printf("Input line %d -- %s\n", l, line)
+		}
 	},
 }
 
