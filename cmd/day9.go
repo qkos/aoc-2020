844d920ad5cb5a0835642bea2d2cff2bbe117f2e
Qamal Kosim-Satyaputra <qamal@deputy.com>
2020-12-09 21:27:46 +1100

    Day 9

--- cmd/day9.go
+++ cmd/day9.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -19,10 +20,69 @@ var day9Cmd = &cobra.Command{
 			panic(err)
 		}
 
+		max := 25 // max
+		var nums []int
+		var invalid int
 		for l, line := range lines {
-			fmt.Printf("Input line %d -- %s\n", l, line)
+
+			num, err := strconv.Atoi(line)
+			if err != nil {
+				panic("not a number")
+			}
+
+			// compute
+			if l >= max {
+				can := false
+				last := nums[len(nums)-max:]
+				valid := toMap(last)
+				for _, v := range last {
+					diff := num - v
+					if _, ok := valid[diff]; ok {
+						can = true
+					}
+				}
+				if !can {
+					invalid = num
+				}
+			}
+
+			nums = append(nums, num)
+
 		}
+
+		minmax := 0
+		for i := 0; i < len(nums)-1; i++ {
+			sum := nums[i]
+			min := sum
+			max := sum
+			// find a contiguous sum
+			for j := i + 1; j < len(nums); j++ {
+				sum += nums[j]
+				if nums[j] < min {
+					min = nums[j]
+				}
+				if nums[j] > max {
+					max = nums[j]
+				}
+				if sum == invalid {
+					minmax = min + max
+					break
+				}
+			}
+		}
+
+		fmt.Printf("Part 1: %d\n", invalid)
+		fmt.Printf("Part 2: %d\n", minmax)
+
 	},
+}
+
+func toMap(list []int) map[int]int {
+	mp := map[int]int{}
+	for _, v := range list {
+		mp[v] = 1
+	}
+	return mp
 }
 
 func init() {
