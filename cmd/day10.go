b22f696746fe6aa0de84fc478e9c21913794611c
Qamal Kosim-Satyaputra <qamal@deputy.com>
2020-12-10 23:29:32 +1100

    Day 10

--- cmd/day10.go
+++ cmd/day10.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"sort"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -19,10 +21,63 @@ var day10Cmd = &cobra.Command{
 			panic(err)
 		}
 
-		for l, line := range lines {
-			fmt.Printf("Input line %d -- %s\n", l, line)
+		var jolts []int
+		joltsMap := map[int]int{}
+		for _, line := range lines {
+			jolt, _ := strconv.Atoi(line)
+			jolts = append(jolts, jolt)
+			joltsMap[jolt] = 0
 		}
+
+		sort.Ints(jolts)
+		mj := jolts[len(jolts)-1] // max
+		jolts = append(jolts, mj+3)
+
+		diff := map[int]int{1: 0, 2: 0, 3: 0}
+		start := 0
+		for _, j := range jolts {
+			if j < start {
+				continue
+			}
+			for k := range diff {
+				if start+k == j {
+					diff[k]++
+					//fmt.Printf("%d choose %d - diff %d\n", start, j, k)
+					start += k
+				}
+			}
+		}
+
+		cnt := comb(map[int]int{}, 0, 0, jolts)
+
+		//fmt.Printf("Max: %d\n", mj)
+		//fmt.Printf("Diffs: %#v\n", diff)
+		fmt.Printf("part1: %d\n", diff[3]*diff[1])
+		fmt.Printf("part2: %d\n", cnt)
 	},
+}
+
+func comb(mem map[int]int, start, i int, ls []int) (count int) {
+	if i == len(ls)-1 {
+		return 1 // last one
+	}
+	for ; i < len(ls); i++ {
+		v := ls[i]
+		diff := v - start
+		if diff > 3 {
+			break
+		}
+		if diff > 0 && diff < 4 {
+			if c, ok := mem[i]; ok {
+				count += c
+			} else {
+				mem[i] = comb(mem, v, i, ls)
+				count += mem[i]
+
+			}
+		}
+	}
+	return count
 }
 
 func init() {
